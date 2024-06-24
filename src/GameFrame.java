d014b70737aeda56daed597129e27d60d0826604
國島聡一郎 <kunishima0912@gmail.com>
2024-06-24 11:34:26 +0900

    練習問題12-1,12-2の追加

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -76,7 +76,7 @@ public class GameFrame extends MyFrame {
 		}
 	}
 	public boolean checkHit(Character a,Character b) {
-		if(Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30) {
+		if(Math.abs(a.x-b.x)<=15 && Math.abs(a.y-b.y)<=15) {
 			return true;
 		}else {
 			return false;
