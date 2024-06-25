074ec7666016c0615d17bda37f3dd438c7e545eb
國島聡一郎 <kunishima0912@gmail.com>
2024-06-25 09:50:32 +0900

    Java13.4の追加

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,11 +4,14 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
-		GameWorld.playerBullets=new Vector<PlayerBullet>();
-		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
-		GameWorld.enterPressed=false;
 		while(true) {
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
+		    GameWorld.playerBullets=new Vector<PlayerBullet>();
+		    GameWorld.enemies=new Vector<Enemy>();
+		    GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		    GameWorld.enterPressed=false;
+		    while(true) {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
@@ -30,6 +33,7 @@ public class GameFrame extends MyFrame {
 				}
 			}
 			sleep(0.03);
+		    }
 		}
 		
 	}
